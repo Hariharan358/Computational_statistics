94ae321d308051ef4bb22b75aa59a39d946b2f0c
Hariharan358 <231401032@rajalakshmi.edu.in>
2024-11-11 10:05:32 +0530

    Update ex 1.py

--- ex 1.py
+++ ex 1.py
@@ -4,17 +4,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 import pandas as pd
-
 # Load the Iris dataset
 df = px.data.iris()
-
 # Select the 'sepal_length' attribute
 attribute = 'sepal_length'
-
 # 1. Print the first five records of the selected attribute
 print("1. Printing the First Five Records:")
 print(df.head())
-
 # 2. Plot the Histogram and Kernel Density Estimation for the attribute 'sepal_length'
 print("2. Print the Histogram and Kernel Density Estimation for the attribute 'sepal_length'")
 sns.histplot(df[attribute], kde=True)
